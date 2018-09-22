39f07bb9ac150df3c0b6ffce9d9ec260e8b6fd5f
Galal Galal <ggalal@umich.edu>
2018-09-22 09:45:02 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ setup(
     name="lungs_finder",
     version="1.0.0",
     description="Library that helps you to find lungs on chest X-ray (CXR) images for further processing",
-    author="Maksym Kholiavchenko",
-    author_email="dirtmaxim@gmail.com",
-    url="https://github.com/dirtmaxim/lungs-finder",
+    author="Galal Galal adapted from Maksym Kholiavchenko",
+    author_email="ggalal@gumich.edu",
+    url="https://github.com/ggalal/lungs-finder",
     license="Apache-2.0",
     packages=find_packages(),
     package_data={"lungs_finder": ["*.xml", "*.np"]}

5d4873cd8b2b03217e4db08bf26bd9517f44402e
Galal Galal <ggalal@umich.edu>
2018-09-23 11:01:54 -0500

    fixed mask bug

--- lungs_finder/find_tools.py
+++ lungs_finder/find_tools.py
@@ -19,7 +19,7 @@ def find_max_rectangle(rectangles):
 
 
 def get_lungs(image, padding=15):
-    mask = np.zeros(image.shape)
+    mask = np.ones(image.shape)
     right_lung = hog_finder.find_right_lung_hog(image)
     left_lung = hog_finder.find_left_lung_hog(image)
 
@@ -86,5 +86,5 @@ def get_lungs(image, padding=15):
 
     top_y = min(y_right, y_left)
     bottom_y = max(y_right + height_right, y_left + height_left)
-    mask[top_y:bottom_y, x_right:x_left + width_left] = np.ones(mask[top_y:bottom_y, x_right:x_left + width_left].shape)
+    mask[top_y:bottom_y, x_right:x_left + width_left] = 1
     return mask
